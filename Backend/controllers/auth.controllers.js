c643573f199f2a4fc1b510eb007c5806f450aff1
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2025-02-16 10:24:23 +0530

    update user route done for image upload

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -76,6 +77,29 @@ export const logout = (req, res, next) => {
   try {
     res.cookie("access_token", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out Successfuly" });
+  } catch (error) {
+    console.log("Error in Logout Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const updateProfile = async (rew, res, next) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user_id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile Pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
   } catch (error) {
     console.log("Error in Logout Controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouts from "./routes/Auth.routes.js";
 import { connectDB } from "./lib/Database.js";
+import { cookieParser } from "cookie-parser";
 
 const app = express();
 
@@ -10,6 +11,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth/", authRouts);
 

0adb73e44110acbd1147795454aadbf446726bff
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2025-02-17 19:25:26 +0530

    Settingup the frountend and dasyui (components library) Created basic routes

--- Frountend/src/App.jsx
+++ Frountend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl">Project Initiated</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
   );
 }
---
+++ Frountend/src/pages/SettingsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SettingsPage() {
+  return <div>setting</div>;
+}
+
+export default SettingsPage;

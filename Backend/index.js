276ce2a6a38884761776313bfcce014553c4bc2d
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2025-02-17 06:16:08 +0530

    creating message module and the end point of the message sending and reciving feature excluding socket.io

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouts from "./routes/Auth.routes.js";
+import messageRouts from "./routes/message.routes.js";
 import { connectDB } from "./lib/Database.js";
 import { cookieParser } from "cookie-parser";
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth/", authRouts);
+app.use("/api/messages/", messageRouts);
 
 app.listen(3000, () => {
   console.log(`Server is running on Port:${PORT}`);

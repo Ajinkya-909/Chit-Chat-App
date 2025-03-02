5dad784b47f2cabde3a0ea273550b760dcc937ef
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2025-03-02 06:43:50 +0530

    Deployment ready app

--- Backend/index.js
+++ Backend/index.js
@@ -6,10 +6,13 @@ import { connectDB } from "./lib/Database.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.io.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +25,13 @@ app.use(
 
 app.use("/api/auth/", authRouts);
 app.use("/api/messages/", messageRouts);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frountend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frountend", "dist", "index.html"));
+  });
+}
 
 server.listen(3000, () => {
   console.log(`Server is running on Port:${PORT}`);
--- Frountend/src/store/useAuthStore.js
+++ Frountend/src/store/useAuthStore.js
@@ -3,6 +3,9 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
@@ -86,7 +89,7 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) {
       return;
     }
-    const socket = io("http://localhost:3000", {
+    const socket = io(BASE_URL, {
       query: { userId: authUser._id },
     });
     socket.connect();
